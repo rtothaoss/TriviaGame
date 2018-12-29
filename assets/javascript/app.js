c37c4dff64f1afacea4bcaa51745e512745fb0a1
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2018-12-29 14:45:06 -0600

    unanswered is logged now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,62 +32,100 @@ function check() {
     console.log(question7);
     console.log(question8);
 
+
+// Question 1 Logic
+
     if (question1 === "Goodfellas") {
         correct++;
     }
-    else if (question1 !== "Goodfellas") {
-        incorrect++;
-    }
-    else if (question1 == "") {
+    else if (question1 === "") {
         unanswered++;
     }
+    else  {
+        incorrect++;
+    }
+
+// Question 2 Logic
     
     if (question2 === "The Mighty Morphin Power Rangers") {
         correct++;
     }
-    else if(question2 !== "The Mighty Morphin Power Rangers") {
+    else if (question2 === "") {
+        unanswered++;
+    }
+    else {
         incorrect++;
     }
-    
+ 
+// Question 3 Logic
+
     if (question3 === "A Few Good Men") {
         correct++;
     }
-    else if (question3 !== "A Few Good Men") {
+    else if (question3 === "") {
+        unanswered++;
+    }
+    else  {
         incorrect++;
     }
-    
+  
+// Question 4 Logic
+
     if (question4 === "Boy Meets World") {
         correct++;
     }
-    else if (question4 !== "Boy Meets World") {
+    else if (question4 === "") {
+        unanswered++;
+    }
+    else {
         incorrect++;
     }
-    
+
+// Question 5 Logic
+
     if (question5 === "Crash") {
         correct++;
     }
-    else if (question5 !== "Crash") {
+    else if (question5 === "") {
+        unanswered++;
+    }
+    else {
         incorrect++;
     }
-    
+ 
+// Quesiton 6 Logic
+
     if (question6 === "Tamagotchi") {
         correct++;
     }
-    else if (question6 !== "Tamagotchi") {
+    else if (question6 === "") {
+        unanswered++;
+    }
+    else  {
         incorrect++;
     }
-    
+ 
+// Quesiton 7 Logic
+
     if (question7 === "Elmo") {
         correct++;
     }
-    else if (question7 !== "Elmo") {
+    else if (question7 === "") {
+        unanswered++;
+    }
+    else  {
         incorrect++;
     }
+
+// Question 8 Logic
 
     if (question8 === "Home Improvement") {
         correct++;
     }
-    else if (question8 !== "Home Improvement") {
+    else if (question8 === "") {
+        unanswered++;
+    }
+    else  {
         incorrect++;
     }
 
@@ -99,8 +137,12 @@ function check() {
     // document.getElementById("after_submit").style.visibility = "visible";
     $("#after_submit").css("visibility", "visible");
 
+// Finished Button 
+    
     $("#number_correct").text("Number Correct: " + correct);
     $("#number_incorrect").text("Number Incorrect: " + incorrect);
+    $("#unanswered").text("Number Unanswered: " + unanswered);
+
 
 }
 
